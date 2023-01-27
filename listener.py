8cb86ee030b0f48dcd3b10b9c91fb9fdafd4f228
bry4 <bvargascardenas@gmail.com>
2023-01-27 17:21:12 -0500

    test 2

--- listener.py
+++ listener.py
@@ -8,17 +8,28 @@ from pymysqlreplication.row_event import (
     WriteRowsEvent,
 )
 
+def get_secrets():
+
+    with open('config/env.json', 'r') as env_file:
+        data = json.load(env_file)
+
+    return data
+
+
 def main():
 
+    key = get_secrets()
+
     MYSQL_SETTINGS = {
-      "host": "",
-      "port": 3306,
-      "user": "",
-      "passwd": ""
+      "host": key["host"],
+      "port": key["port"],
+      "user": key["user"],
+      "passwd": key["pwd"]
       }
 
     print(">>>listener start streaming to:mysql_data")
     stream = BinLogStreamReader(connection_settings=MYSQL_SETTINGS,
+                                server_id = 100,
                                 blocking=True,
                                 resume_stream=True,
                                 only_events=[DeleteRowsEvent, WriteRowsEvent, UpdateRowsEvent])
